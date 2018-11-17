d21442a92067fc6c645347937aa647141fe63894
tpc707494 <1679867915@qq.com>
2018-11-17 16:58:21 +0800

    修改

--- thinkphp/library/think/cache/driver/File.php
+++ thinkphp/library/think/cache/driver/File.php
@@ -123,6 +123,7 @@ class File extends Driver
                 //启用数据压缩
                 $content = gzuncompress($content);
             }
+            
             $content = unserialize($content);
             return $content;
         } else {
